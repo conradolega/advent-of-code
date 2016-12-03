403194f5b4a22b8971e46d2bd98f2ecfd370a870
conradolega <conradolega@gmail.com>
2016-12-03 14:06:53 +0800

    Add solution to day 2, part 2

--- 2016/day2.py
+++ 2016/day2.py
@@ -16,3 +16,24 @@ for step in steps:
     code += str(start)
 
 print(code)
+
+keypad = 'XX1XXX234X56789XABCXXXDXX'
+code = ''
+start = 10 # position of 5
+
+for step in steps:
+    for direction in step:
+        if direction == 'U':
+            movement = -5
+        elif direction == 'D':
+            movement = 5
+        elif direction == 'L':
+            movement = -1
+        elif direction == 'R':
+            movement = 1
+
+        if start + movement >= 2 and start + movement <= 22 and keypad[start + movement] != 'X':
+            start += movement
+    code += str(keypad[start])
+
+print(code)
