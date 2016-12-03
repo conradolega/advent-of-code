2d5e8fe622ec35eb8ac3dc1258c26720c42f1982
conradolega <conradolega@gmail.com>
2016-12-03 13:32:54 +0800

    Add solution to day 1, part 2

--- 2016/day1.py
+++ 2016/day1.py
@@ -5,15 +5,32 @@ x = 0
 y = 0
 sequence = [0, -1, 0, 1]
 direction = (0, 3)
+visited = []
+first_visited = []
+
+def check_if_visited(x, y):
+    if first_visited == [] and (x, y) in visited:
+        first_visited.append((x, y))
+    else:
+        visited.append((x, y))
+
+
 for index, step in enumerate(steps):
     if step[0] == 'L':
         direction = ((direction[0] - 1) % 4, (direction[1] - 1) % 4)
     else:
         direction = ((direction[0] + 1) % 4, (direction[1] + 1) % 4)
 
+    length = int(step[1:])
     if sequence[direction[0]] != 0:
-        x += int(step[1:]) * sequence[direction[0]]
+        for i in range(0, length):
+            check_if_visited(x + sequence[direction[0]] * i, y)
+        x += int(length) * sequence[direction[0]]
     else:
-        y += int(step[1:]) * sequence[direction[1]]
+        for i in range(0, length):
+            check_if_visited(x, y + sequence[direction[1]] * i)
+        y += int(length) * sequence[direction[1]]
+
 
 print(abs(x) + abs(y))
+print(abs(first_visited[0][0]) + abs(first_visited[0][1]))
